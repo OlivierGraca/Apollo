fa5264fa74409df7db31f34b701583237298d597
OlivierGraca <202380025@cegepstfe.ca>
2023-07-04 09:45:36 -0400

    Prise en charge du slider

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,4 +1,55 @@
 $( document ).ready(function() {
+
+// Déclaration d'une variable pour situer le Cégep
+var stfe = [48.64979, -72.45859];
+
+// Déclaration de la carte
+var map = L.map('map').setView(stfe, 10);
+
+// Déclaration du calque de base
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 10,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+// Ajout du marqueur
+var marker = L.marker(stfe).addTo(map);
+
+// Ajout d'une infobulle 
+marker.bindPopup("<h3>St-Félicien, Quebec.</h3>");
+
+// Gestion du slider
+	const prevBtn = document.querySelector('.prev');
+	const nextBtn = document.querySelector('.next');
+
+	const slides = document.querySelectorAll('.banner li');
+	let currentSlide = 0;
+	
+	prevBtn.addEventListener('click', function() {
+		currentSlide--
+		if (currentSlide < 0) {
+			currentSlide = slides.length -1;
+		}
+
+		showSlide(currentSlide);
+	});
+	
+	nextBtn.addEventListener('click', function() {
+		currentSlide++
+		if (currentSlide > slides.length -1) {
+			currentSlide = 0;
+		}
+
+		showSlide(currentSlide);
+	});
+
+	function showSlide(slide) {
+		slides.forEach((slide) => {
+		  slide.classList.remove('active');
+		});
+	  
+		slides[slide].classList.add('active');
+	  }
 	
 function nav(){
 	
@@ -13,21 +64,3 @@ function nav(){
 nav();
 
 });
-
-// Déclaration d'une variable pour situer MTL
-var mtl = [45.5031824, -73.5698065];
-
-// Déclaration de la carte
-var map = L.map('map').setView(mtl, 10);
-
-// Déclaration du calque de base
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 10,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-
-// Ajout du marqueur
-var marker = L.marker(mtl).addTo(map);
-
-// Ajout d'une infobulle 
-marker.bindPopup("<h3>Montreal, Quebec.</h3>");
